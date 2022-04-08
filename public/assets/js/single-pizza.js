dd2fa87443dbb2ee23cef447f714aba065d5c962
mattsiis <35969076+mattsiis@users.noreply.github.com>
2022-04-08 00:09:08 -0400

    finish reply comment

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -129,6 +129,28 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok){
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function handleNewCommentSubmit(event) {
@@ -172,5 +194,8 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+$backBtn.addEventListener('click', function() {
+  window.history.back();
+})
 
 getPizza();
